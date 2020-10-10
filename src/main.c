5301126c74f8f4e8debf466c2f829ee185f25dfe
Alma Elphias <igormitchell11@gmail.com>
2020-10-10 20:29:37 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -74,7 +74,7 @@ int		main(int argc, char **argv)
 	if (argc < 2)
 	{
 		write(2, "usage: ./fractol [flag]\nm - mandelbrot set\n"
-		"j julia set\nr - 3rd set\n", 67);
+		"j julia set\nb - burning ship\n", 79);
 		return (1);
 	}
 	if (argv[1][0] != 'm' && argv[1][0] != 'j' && argv[1][0] != 'b')
